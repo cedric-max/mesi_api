295f493c8678d5cefb7d1efbd12987cac77a2bcb
cedric kadima <kadimacedric3@gmail.com>
2022-06-22 14:10:22 +0200

    add new theme

---
+++ front-end/src/views/Profile.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <p>
+      email : {{user.email}}
+    </p>
+    <p>
+      role : {{user.roles}}
+    </p>
+    <p>
+      psw : {{user.password}}
+    </p>
+    <p>
+      firstName : {{user.firstName}}
+    </p>
+    <p>
+      lastName : {{user.username}}
+    </p>
+    <p>
+      adress : {{user.address}}
+    </p>
+    <p>
+      phoneNumber : {{user.phoneNumber}}
+    </p>
+    <p>
+      city : {{user.city}}
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "Profile",
+  data () {
+    return {
+      user: ""
+    }
+  },
+  created() {
+    axios
+        .get('https://127.0.0.1:8000/api/users?page=1')
+        .then( response => {
+          console.log(response.data["hydra:member"][0])
+          this.user = response.data["hydra:member"][0]
+        })
+        .catch(e => {
+          console.log(e)
+        })
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
