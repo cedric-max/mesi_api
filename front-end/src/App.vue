32def17f6ba7fa846d6de7298bed49db4df50872
cedric kadima <kadimacedric3@gmail.com>
2022-07-02 21:53:46 +0200

    add sticky navbar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
-    <nav>
+    <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/about">About</b-nav-item>
+          <b-nav-item to="/profile">Profile</b-nav-item>
+          <b-nav-item to="/listArticles">Articles</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item href="#" disabled class="ml-auto">Conection</b-nav-item>
+          <b-nav-item href="#" disabled class="ml-auto">Cart</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <!--<nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/profile">Profile</router-link> |
       <router-link to="/listArticles">Articles</router-link>
-    </nav>
+    </nav>-->
     <router-view />
   </div>
 </template>
@@ -17,18 +31,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/Entity/Color.php
+++ src/Entity/Color.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=ColorRepository::class)
  */
 #[ApiResource]
