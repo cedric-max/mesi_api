ee191a604d8de0d3a2400b6e5d9d8d4f12bad8bf
seb <sebastien.kemmoun@ipilyon.net>
2022-07-02 16:16:28 +0200

    add entity brand and relation between brand_shoe

---
+++ src/Entity/Brand.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\BrandRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=BrandRepository::class)
+ */
+#[ApiResource]
+class Brand
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Shoe::class, mappedBy="shoeBrand", orphanRemoval=true)
+     */
+    private $shoes;
+
+    public function __construct()
+    {
+        $this->shoes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Shoe>
+     */
+    public function getShoes(): Collection
+    {
+        return $this->shoes;
+    }
+
+    public function addShoe(Shoe $shoe): self
+    {
+        if (!$this->shoes->contains($shoe)) {
+            $this->shoes[] = $shoe;
+            $shoe->setShoeBrand($this);
+        }
+
+        return $this;
+    }
+
+    public function removeShoe(Shoe $shoe): self
+    {
+        if ($this->shoes->removeElement($shoe)) {
+            // set the owning side to null (unless already changed)
+            if ($shoe->getShoeBrand() === $this) {
+                $shoe->setShoeBrand(null);
+            }
+        }
+
+        return $this;
+    }
+}
